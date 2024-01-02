4180e847d0298dd479ff05b7eb8663ca6b2eda58
jajana98 <tornikejajanidze11@gmail.com>
2024-01-02 05:44:42 +0100

    added steps count to solution

--- theoritical-informatics-v1.pl
+++ theoritical-informatics-v1.pl
@@ -1,20 +1,8 @@
-%dataset 1
-cell(1, 1, open).
-cell(1, 2, blocked).
-cell(1, 3, blocked).
-cell(1, 4, blocked).
-cell(2, 1, open).
-cell(2, 2, open).
-cell(2, 3, open).
-cell(2, 4, open).
-cell(3, 1, blocked).
-cell(3, 2, blocked).
-cell(3, 3, open).
-cell(3, 4, open).
-cell(4, 1, open).
-cell(4, 2, open).
-cell(4, 3, blocked).
-cell(4, 4, open).
+% Define the maze layout.
+cell(1, 1, open). cell(1, 2, blocked). cell(1, 3, blocked). cell(1, 4, blocked).
+cell(2, 1, open). cell(2, 2, open). cell(2, 3, open). cell(2, 4, open).
+cell(3, 1, blocked). cell(3, 2, blocked). cell(3, 3, open). cell(3, 4, open).
+cell(4, 1, open). cell(4, 2, open). cell(4, 3, blocked). cell(4, 4, open).
 
 % Define the start and end points of the maze.
 start(1, 1).
@@ -31,12 +19,17 @@ valid_move(X, Y, Visited) :-
     cell(X, Y, open),
     \+ member((X, Y), Visited).
 
-% Solve the maze using depth-first search.
-solve(X, Y, Path, Path) :- end(X, Y).  % Reached the end
-solve(X, Y, Visited, Path) :-
+% Solve the maze using depth-first search with a step counter.
+solve(X, Y, Visited, Path, Steps) :-
+    end(X, Y),  % Reached the end
+    length(Visited, L), 
+    Steps is L - 1,  % Subtracting 1 because the starting cell isn't a step.
+    Path = Visited.
+
+solve(X, Y, Visited, Path, Steps) :-
     move(X, Y, X1, Y1),  % Try a move.
     valid_move(X1, Y1, Visited),  % Check if the move is valid.
-    solve(X1, Y1, [(X1, Y1)|Visited], Path).  % Continue from the new position.
+    solve(X1, Y1, [(X1, Y1)|Visited], Path, Steps).  % Continue from the new position.
 
 % Print each cell of the maze with ASCII characters.
 print_cell(X, Y, Path) :- member((X, Y), Path), write('X '), !. % Path cell
@@ -45,19 +38,20 @@ print_cell(X, Y, _) :- end(X, Y), write('E '), !.  % End cell
 print_cell(X, Y, _) :- cell(X, Y, blocked), write('# '), !.  % Blocked cell
 print_cell(_, _, _) :- write('. '), !.  % Open cell
 
-% Print the entire maze with the path.
-print_maze_with_path(MaxX, MaxY, Path) :-
-    start(StartX, StartY),
-    solve(StartX, StartY, [(StartX, StartY)], Path),
-    reverse(Path, RevPath),  % Reverse the path to get the correct order
-    between(1, MaxX, X),
-    between(1, MaxY, Y),
-    print_cell(X, Y, RevPath),
-    (Y == MaxY -> nl ; true),  % New line at the end of each row
-    (X == MaxX, Y == MaxY -> nl ; true),  % Extra new line at the end of the maze
-    fail.  % Continue to the next cell
-print_maze_with_path(_, _).
+% Print the entire maze with the path and steps.
+print_maze_with_path(MaxX, MaxY, Path, Steps) :-
+    forall(between(1, MaxX, X),
+           (forall(between(1, MaxY, Y),
+                    print_cell(X, Y, Path)),
+            nl)),  % New line at the end of each row
+    write('Number of steps to solve: '), write(Steps), nl.
 
-% Find and print the path through the maze.
-find_and_print_path :-
-    print_maze_with_path(4, 4, _).
+% Find and print the path through the maze with the least number of steps.
+find_shortest_path :-
+    start(StartX, StartY),
+    solve(StartX, StartY, [(StartX, StartY)], Path, Steps),
+    reverse(Path, RevPath),  % Reverse the path to get the correct order
+    print_maze_with_path(4, 4, RevPath, Steps).
+
+% Initiate the search for the shortest path.
+:- find_shortest_path.
